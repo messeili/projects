97f3139e045ec77a1a62224ab715c794c03e0efd
messeili <mymoa.2014@gmail.com>
2020-06-24 12:06:05 +0300

    add some magic

--- js/javas.js
+++ js/javas.js
@@ -13,17 +13,11 @@ function commit() {
     if (comitt == "yes"){
     alert ('Thanks for your awarness')
     }else {
-        prompt('you should follow them!!!')
+    alert('you should follow them!!!')
     }
 }
-function clean(){
-    var clean = prompt('How many times do you wash your hand daily??', 'please enter a number');
-    if (clean >=10){
-    alert (clean +' Times is good \,Give me a high five!!!')
-    }else{
-        alert (clean + ' times is not enough Go wash your self!!!!!')
-    }
-}
+
+
 function reuslt() {
     var result = confirm("Please confirm to follow Public safety measures");
     if (result==true){
@@ -34,9 +28,17 @@ function reuslt() {
         document.write( '<p class="not">' + status + '</p>');
     }
 }
+function clean(){
+    var clean = prompt('How many times do you wash your hand daily??', 'please enter a number');
+    if (clean >=10){
+    alert (clean +' Times is good \,Give me a high five!!!')
+    }else{
+        alert (clean + ' times is not enough Go wash your self!!!!! GETOUT from my website hahahahaha');
+        window.close();
+    }
+}
 
 welcome()
 commit()
-clean()
 reuslt()
-
+clean()
